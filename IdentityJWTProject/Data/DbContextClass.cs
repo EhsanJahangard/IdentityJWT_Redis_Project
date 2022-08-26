962a062b5cfccf3599d3d3ff541e4dc77c5e2e9e
Ehsan Jahangard <ehsanjahangard2020@gmail.com>
2022-08-26 21:39:05 +0430

    Add project files.

---
+++ IdentityJWTProject/Data/DbContextClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IdentityJWTProject.Models;
+namespace IdentityJWTProject.Data
+{
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+        public DbContextClass(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(_configuration.GetConnectionString("JWTContextConnection"));
+        }
+        public DbSet<Product> Products
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ IdentityJWTProject/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿namespace IdentityJWTProject.Models
+{
+    public class Login
+    {
+        public string? UserName
+        {
+            get;
+            set;
+        }
+        public string? Password
+        {
+            get;
+            set;
+        }
+    }
+}
